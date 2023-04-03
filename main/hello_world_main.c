3f8f608d161eb31d5dd09b050f17543864a1c9ff
crisqifawei <329410527@qq.com>
2023-04-04 00:47:43 +0800

    first commit

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -15,37 +15,19 @@
 #include "i2c_manager.h"
 #include "lvgl_helpers.h"
 
-#if LV_BUILD_EXAMPLES && LV_USE_SLIDER
-
-static lv_obj_t * label;
-
-static void slider_event_cb(lv_event_t * e)
-{
-    lv_obj_t * slider = lv_event_get_target(e);
-
-    /*Refresh the text*/
-    lv_label_set_text_fmt(label, "%d", lv_slider_get_value(slider));
-    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
-}
-
 /**
- * Create a slider and write its value on a label.
+ * Create a button with a label and react on click event.
  */
-void lv_example_get_started_3(void)
+void lv_example_get_started_1(void)
 {
-    /*Create a slider in the center of the display*/
-    lv_obj_t * slider = lv_slider_create(lv_scr_act());
-    lv_obj_set_width(slider, 200);                          /*Set the width*/
-    lv_obj_center(slider);                                  /*Align to the center of the parent (screen)*/
-    lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
-
-    /*Create a label below the slider*/
-    label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "0");
-    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    if (btn) {
+      lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+      lv_obj_set_size(btn, 0, 50);                          /*Set its size*/
+    } else {
+      printf("lv_btn_create error!\n");
+    }
 }
-
-#endif
 
 
 void app_main(void)
@@ -71,13 +53,15 @@ void app_main(void)
         return;
     }
 
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
     //lvgl_i2c_locking(i2c_manager_locking());
     lv_init();
     lvgl_driver_init();
 
     printf("lv_example_get_started_2...start...\n");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //lv_example_get_started_3();
+    //lv_example_get_started_1();
     printf("lv_example_get_started_2...end... %s,%d\n", lv_version_info(),lv_version_patch());
 
     printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
